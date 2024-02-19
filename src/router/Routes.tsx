9e96500fea3d53f20fa1635f50699f2d42a4c695
Hasaka <h.s.s.malalasekara@gmail.com>
2024-02-19 08:09:35 +0530

    add lazy loading

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -1,19 +1,41 @@
+import { lazy, Suspense } from "react";
 import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
+
 import AppLoadingPage from "../pages/appLoadingPage/AppLoadingPage";
 import ErrorPage from "../pages/errorPage/ErrorPage";
 import HomePage from "../pages/homePage/HomePage";
-import SearchPage from "../pages/searchPage/SearchPage";
 import FavoritePage from "../pages/favoritePage/FavoritePage";
+import BouncingLoadingIndicator from "../components/BouncingLoadingIndicator/BouncingLoadingIndicator";
+
+import { Route as NavRoute } from "../constants/routes-enum";
+import { NavigationService } from "../services";
+
+const SearchPage = lazy(() => import("../pages/searchPage/SearchPage"));
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<AppLoadingPage />} errorElement={<ErrorPage />}>
       <Route index element={<HomePage />} />
-      <Route path="search" element={<SearchPage />} />
+      <Route
+        path="search"
+        element={
+          <ErrorBoundary
+            FallbackComponent={ErrorPage}
+            onReset={() => {
+              NavigationService.navigate(NavRoute.Home);
+            }}
+          >
+            <Suspense fallback={<BouncingLoadingIndicator />}>
+              <SearchPage />
+            </Suspense>
+          </ErrorBoundary>
+        }
+      />
       <Route path="favorite" element={<FavoritePage />} />
       <Route path="*" element={<ErrorPage />} />
     </Route>
